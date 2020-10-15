45f95ac667504c529243b1a460006cf28a17da39
Sebastian <sebastian.utbult@elev.ga.ntig.se>
2020-10-15 13:12:21 +0200

    js

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,14 @@
-function colorChange(){
-    document.getElementById
+
+
+function hover(x){
+    console.log(x)
+    if(x){
+        document.getElementById("searchbar").style.backgroundColor = "white";
+        document.getElementById("search").style.color = "black";
+        document.getElementById("icon").style.color = "black";
+    }else{
+        document.getElementById("searchbar").style.backgroundColor = "rgb(221, 116, 116)";
+        document.getElementById("search").style.color = "white";
+        document.getElementById("icon").style.color = "white";
+    }
 }
